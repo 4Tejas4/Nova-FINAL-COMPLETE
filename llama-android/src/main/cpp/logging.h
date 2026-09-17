1a05e9f34fea50644d3d30976b7869213d08e8ef
4Tejas4 <channelyoutube042@gmail.com>
2026-09-17 18:32:07 +0530

    Update logging.h

--- llama-android/src/main/cpp/logging.h
+++ llama-android/src/main/cpp/logging.h
@@ -1,61 +1,50 @@
-//
-// Created by Han Yin on 10/31/25.
-//
-
-#ifndef AICHAT_LOGGING_H
-#define AICHAT_LOGGING_H
-
-#endif //AICHAT_LOGGING_H
-
 #pragma once
+
 #include <android/log.h>
 
 #ifndef LOG_TAG
-#define LOG_TAG "ai-chat"
+#define LOG_TAG "Nova"
 #endif
 
 #ifndef LOG_MIN_LEVEL
-#if defined(NDEBUG)
 #define LOG_MIN_LEVEL ANDROID_LOG_INFO
-#else
-#define LOG_MIN_LEVEL ANDROID_LOG_VERBOSE
-#endif
 #endif
 
-static inline int ai_should_log(int prio) {
-    return __android_log_is_loggable(prio, LOG_TAG, LOG_MIN_LEVEL);
+namespace nova {
+
+inline bool is_loggable(int prio) {
+    /*
+     * __android_log_is_loggable() was introduced in Android API 30.
+     * Nova supports lower API levels, so don't call that API here.
+     *
+     * Android log priorities increase in severity:
+     * VERBOSE < DEBUG < INFO < WARN < ERROR < FATAL
+     *
+     * Keep messages at or above the configured minimum level.
+     */
+    return prio >= LOG_MIN_LEVEL;
 }
 
-#if LOG_MIN_LEVEL <= ANDROID_LOG_VERBOSE
-#define LOGv(...) do { if (ai_should_log(ANDROID_LOG_VERBOSE)) __android_log_print(ANDROID_LOG_VERBOSE, LOG_TAG, __VA_ARGS__); } while (0)
-#else
-#define LOGv(...) ((void)0)
-#endif
+inline void log_print(
+        int prio,
+        const char* tag,
+        const char* message) {
 
-#if LOG_MIN_LEVEL <= ANDROID_LOG_DEBUG
-#define LOGd(...) do { if (ai_should_log(ANDROID_LOG_DEBUG)) __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__); } while (0)
-#else
-#define LOGd(...) ((void)0)
-#endif
-
-#define LOGi(...)   do { if (ai_should_log(ANDROID_LOG_INFO )) __android_log_print(ANDROID_LOG_INFO , LOG_TAG, __VA_ARGS__); } while (0)
-#define LOGw(...)   do { if (ai_should_log(ANDROID_LOG_WARN )) __android_log_print(ANDROID_LOG_WARN , LOG_TAG, __VA_ARGS__); } while (0)
-#define LOGe(...)   do { if (ai_should_log(ANDROID_LOG_ERROR)) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__); } while (0)
-
-static inline int android_log_prio_from_ggml(enum ggml_log_level level) {
-    switch (level) {
-        case GGML_LOG_LEVEL_ERROR: return ANDROID_LOG_ERROR;
-        case GGML_LOG_LEVEL_WARN:  return ANDROID_LOG_WARN;
-        case GGML_LOG_LEVEL_INFO:  return ANDROID_LOG_INFO;
-        case GGML_LOG_LEVEL_DEBUG: return ANDROID_LOG_DEBUG;
-        default:                   return ANDROID_LOG_DEFAULT;
+    if (!is_loggable(prio)) {
+        return;
     }
+
+    __android_log_write(
+        prio,
+        tag ? tag : LOG_TAG,
+        message ? message : "");
 }
 
-static inline void aichat_android_log_callback(enum ggml_log_level level,
-                                              const char* text,
-                                              void* /*user*/) {
-    const int prio = android_log_prio_from_ggml(level);
-    if (!ai_should_log(prio)) return;
-    __android_log_write(prio, LOG_TAG, text);
+inline void log_print(
+        int prio,
+        const char* message) {
+
+    log_print(prio, LOG_TAG, message);
 }
+
+} // namespace nova
